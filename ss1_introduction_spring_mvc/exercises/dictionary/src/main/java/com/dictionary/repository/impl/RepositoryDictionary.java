0c094b2386de8743b45fd42adf94362775e22220
nguyenduyquang2712 <quangkg8@gmail.com>
2022-10-13 23:54:22 +0700

    update

--- ss1_introduction_spring_mvc/exercises/dictionary/src/main/java/com/dictionary/repository/impl/RepositoryDictionary.java
+++ ss1_introduction_spring_mvc/exercises/dictionary/src/main/java/com/dictionary/repository/impl/RepositoryDictionary.java
@@ -4,12 +4,13 @@ package com.dictionary.repository.impl;
 import com.dictionary.model.DictonaryApp;
 import com.dictionary.repository.IRepositoryDictionary;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+@Repository
 public class RepositoryDictionary implements IRepositoryDictionary {
   private static List<DictonaryApp> dictonaryAppList = new ArrayList<>();
    static {
