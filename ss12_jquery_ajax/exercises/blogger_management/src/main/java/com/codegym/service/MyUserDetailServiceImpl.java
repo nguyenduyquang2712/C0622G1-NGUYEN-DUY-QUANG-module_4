8c4c8e5ab2acfaf608e6bb8af3145fbc990f7701
nguyenduyquang2712 <quangkg8@gmail.com>
2022-11-02 16:41:01 +0700

    update

---
+++ ss12_jquery_ajax/exercises/blogger_management/src/main/java/com/codegym/service/MyUserDetailServiceImpl.java
@@ -0,0 +1,24 @@
+package com.codegym.service;
+
+import com.codegym.model.MyUserDetail;
+import com.codegym.model.User;
+import com.codegym.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User Name: " + username+" not found");
+        }
+        return new MyUserDetail(user);
+    }
+}
